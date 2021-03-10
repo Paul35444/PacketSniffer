a3bccdcbad17e8352f8d828f2128e39254fc0dc6
Paul St. Amour <paul35444@aol.com>
2021-03-10 12:16:36 -0600

    -added args to sniff func -created call back func (process_sniffed_packet)

--- PacketSniffer.py
+++ PacketSniffer.py
@@ -2,6 +2,8 @@
 
 import scapy.all as scapy
 
-def sniff():
-    scapy.sniff()
-
+def sniff(interface):
+    scapy.sniff(iface=interface, store=False, prn=process_sniffed_packet) #store=False arg tells scapy to not store packets
+                                                                          #prn arg allows call back func to be executed every time packet is captured
+def process_sniffed_packet():
+    print(packet)
