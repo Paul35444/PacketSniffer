ccfaae3eabe459266c70e52c52a4cf6fcb781d8a
Paul St. Amour <paul35444@aol.com>
2021-03-10 12:24:44 -0600

    -import scapy.layers lib for http -add if state to check layers for only HTTPRequests

--- PacketSniffer.py
+++ PacketSniffer.py
@@ -1,11 +1,14 @@
 #!/usr/bin/env python3
 
 import scapy.all as scapy
+import scapy.layers import http
 
 def sniff(interface):
     scapy.sniff(iface=interface, store=False, prn=process_sniffed_packet) #store=False arg tells scapy to not store packets
                                                                           #prn arg allows call back func to be executed every time packet is captured
+                                                                                       
 def process_sniffed_packet():
-    print(packet)
+    if packet.haslayer(http.HTTPRequest): #print only HTTPRequest packets
+        print(packet)
 
 sniff("eth0") #sniff interface
