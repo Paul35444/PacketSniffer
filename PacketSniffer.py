9faaf3ed61c65ebfcc7f00edd1ebb4742a3342fa
Paul St. Amour <paul35444@aol.com>
2021-03-11 20:12:24 -0600

    -moved comments

--- PacketSniffer.py
+++ PacketSniffer.py
@@ -4,11 +4,14 @@ import scapy.all as scapy
 import scapy.layers import http
 
 def sniff(interface):
-    scapy.sniff(iface=interface, store=False, prn=process_sniffed_packet) #store=False arg tells scapy to not store packets
-                                                                          #prn arg allows call back func to be executed every time packet is captured
+#store=False arg tells scapy to not store packets
+#prn arg allows call back func to be executed every time packet is captured
+    scapy.sniff(iface=interface, store=False, prn=process_sniffed_packet)
                                                                                        
 def process_sniffed_packet():
-    if packet.haslayer(http.HTTPRequest): #print only HTTPRequest packets
+#print only HTTPRequest packets
+    if packet.haslayer(http.HTTPRequest):
         print(packet)
 
-sniff("eth0") #sniff interface
+ #sniff interface (hardcoded)
+sniff("eth0")
