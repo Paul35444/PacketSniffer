cc7457679d2f631cecf22a9fea0438d867c11992
Paul St. Amour <paul35444@aol.com>
2021-03-24 19:04:50 -0500

    -fixed import of scapy -added scapy_packet to get payload based on IP

--- FileInterceptor/FileInterceptor/FileInterceptor.py
+++ FileInterceptor/FileInterceptor/FileInterceptor.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 import netfilterqueue
-import scapy.all as scapany
+import scapy.all as scapy
 
 def process_packet(packet):
+    scapy_packet = scapy.IP(packet.get_payload()):
