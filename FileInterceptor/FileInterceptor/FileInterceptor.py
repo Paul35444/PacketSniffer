5cc0e1f7d3011421be3fbca3424d7d9cfcb4770d
Paul St. Amour <paul35444@aol.com>
2021-03-24 19:07:10 -0500

    -added if state for packets that only have DNS request responses

--- FileInterceptor/FileInterceptor/FileInterceptor.py
+++ FileInterceptor/FileInterceptor/FileInterceptor.py
@@ -4,3 +4,5 @@ import scapy.all as scapy
 
 def process_packet(packet):
     scapy_packet = scapy.IP(packet.get_payload()):
+#only get packets that have DNS request response
+    if scapy_packet.haslayer(scapy.DNSRR):
